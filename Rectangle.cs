0994042b1a7bce3c96bc6230bcf0156ed0f7e62f
AyushKH <tummysfat@gmail.com>
2025-09-17 00:03:54 +0700

    поч классы фигур в файле с классом точек по другим файлам раскидал

---
+++ Rectangle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba1Fig
+{
+    public class Rectangle
+    {
+        private Points startPoint;
+        private int width;
+        private int height;
+
+        public Rectangle(Points startPoint, int width, int height)
+        {
+            this.startPoint = startPoint;
+            this.width = width;
+            this.height = height;
+        }
+
+        public Points getStartPoint()
+        {
+            return startPoint;
+        }
+
+        public int getWidth()
+        {
+            return width;
+        }
+
+        public int getHeight()
+        {
+            return height;
+        }
+
+        public void addX(int X)
+        {
+            startPoint.addX(X);
+        }
+
+        public void addY(int Y)
+        {
+            startPoint.addY(Y);
+        }
+    }
+}
