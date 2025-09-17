6d2bdcfe1091a8379d396650346d47f51719ca43
Svin <SAPR@PC-NXT>
2025-09-17 16:58:02 +0700

    Сделал методы рисования линий и фигур в общем, пока работает только треугольник

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,7 +24,43 @@ namespace Laba1Fig
         {
             InitializeComponent();
 
+            wcanvas = (int) Polotno.Width;
+            hcanvas = (int) Polotno.Height;
 
+        }
+        Random rand = new Random();
+        int wcanvas, hcanvas;
+
+        private void Tri_Click(object sender, RoutedEventArgs e)
+        {
+            Points p1 = new Points(rand.Next(wcanvas), rand.Next(hcanvas));
+            Points p2 = new Points(rand.Next(wcanvas), rand.Next(hcanvas));
+            Points p3 = new Points(rand.Next(wcanvas), rand.Next(hcanvas));
+
+            Triangle tree = new Triangle(p1, p2, p3);
+
+            DrawFig(new List<Points>() {p1, p2, p3});
+        }
+
+        private void DrawLine(Points a, Points b) 
+        {
+            Line line = new Line
+            {
+                StrokeThickness = 2,
+                Stroke = Brushes.Blue,
+                X1 = a.getX(),
+                Y1 = a.getY(),
+                X2 = b.getX(),
+                Y2 = b.getY()
+            };
+            Polotno.Children.Add(line);
+        }
+
+        private void DrawFig(List<Points> pointz)
+        {
+            for (int i = 0; i < pointz.Count - 1; i++)
+                DrawLine(pointz[i], pointz[i + 1]);
+            DrawLine(pointz[pointz.Count - 1], pointz[0]);
         }
     }
 }
